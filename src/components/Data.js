f7b31010ef457b9cf0940218e3dc41d0953a7e15
Arif <itsmaansari@gmail.com>
2024-01-21 23:16:16 +0530

    basic edit

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,7 +2,6 @@ import cart1 from '../assets/images/burger.png';
 import cart2 from '../assets/images/pizza.png';
 import cart3 from '../assets/images/chips.png';
 import cart4 from '../assets/images/fries.png';
-
 const cart = [
     { img: cart1, },
     { img: cart2, },
