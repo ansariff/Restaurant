f09a6b3606c7aa7a65449e9602a9cf16c66b385a
Arif <itsmaansari@gmail.com>
2024-01-23 20:18:24 +0530

    add image

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,11 +2,13 @@ import cart1 from '../assets/images/burger.png';
 import cart2 from '../assets/images/pizza.png';
 import cart3 from '../assets/images/chips.png';
 import cart4 from '../assets/images/fries.png';
+import cart5 from '../assets/images/cookie.png';
 const cart = [
     { img: cart1, },
     { img: cart2, },
     { img: cart3, },
     { img: cart4, },
+    { img: cart5, },
 ];
 
 export default cart;
